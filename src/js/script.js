ae7edd8df3c3481fab8c4d3390de4ea98f85fcb0
assentorp <pa@tosider.dk>
2019-05-21 16:27:16 +0200

    Added editing functionality

--- src/js/script.js
+++ src/js/script.js
@@ -88,4 +88,13 @@ $(document).ready(function() {
   $('a[data-toggle="tab"]').on("shown.bs.tab", function(e) {
     $.fn.dataTable.tables({ visible: true, api: true }).columns.adjust();
   });
+
+  $(".edit").on("click", function() {
+    $(".readonly").hide();
+    $(".write").show();
+  });
+  $(".save").on("click", function() {
+    $(".readonly").show();
+    $(".write").hide();
+  });
 });
